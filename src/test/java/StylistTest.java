f01ab6d17a71d5eb96f06eb887be9d284c11921b
Scott Holland <scotthollandjr@gmail.com>
2016-05-06 10:50:25 -0700

    Add passing getter tests

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -9,4 +9,16 @@ public class StylistTest {
     Stylist newStylist = new Stylist("Trixy", 15);
     assertEquals(true, newStylist instanceof Stylist);
   }
+
+  @Test
+  public void getName_returnsName_String() {
+    Stylist newStylist = new Stylist("Trixy", 15);
+    assertEquals("Trixy", newStylist.getName());
+  }
+
+  @Test
+  public void getRate_returnsRate_int() {
+    Stylist newStylist = new Stylist("Trixy", 15);
+    assertEquals(15, newStylist.getRate());
+  }
 }
