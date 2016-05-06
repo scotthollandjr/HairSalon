bedb2982362e1ea3baab7de132c7ab125e03ec36
Scott Holland <scotthollandjr@gmail.com>
2016-05-06 10:47:03 -0700

    Add first stylist functionality with passing test

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,5 +1,27 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Stylist {
+  private int id;
+  private String name;
+  private int rate;
+  private List<String> days;
+  private List<Client> clientList;
 
+  public Stylist(String name, int rate) {
+    this.name = name;
+    this.rate = rate;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getRate() {
+    return rate;
+  }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 
 public class StylistTest {
 
-
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist newStylist = new Stylist("Trixy", 15);
+    assertEquals(true, newStylist instanceof Stylist);
+  }
 }
